6a11a1170907ed3f8aba28fdcd5d4d3653cbafcf
stration <34169333+stration@users.noreply.github.com>
2025-03-26 13:18:23 +0300

    Update tracer.py

--- utils/tracer.py
+++ utils/tracer.py
@@ -1,58 +1,60 @@
-# utils/tracer.py
+from django.contrib.contenttypes.models import ContentType
 from dcim.models import Cable, PowerPort, PowerOutlet
 from collections import deque
 
 def trace_cable_path(start_port):
     """
     Трассирует путь кабеля от начального порта до конечной точки
+    Возвращает список сегментов пути
     """
     visited = set()
     path = []
-    queue = deque([(start_port, [])])
+    current_port = start_port
     
-    while queue:
-        current_port, current_path = queue.popleft()
-        
+    while True:
         if current_port.id in visited:
-            continue
-            
+            break
         visited.add(current_port.id)
         
-        # Получаем все кабели, подключенные к текущему порту
-        cables = Cable.objects.filter(
+        # Получаем кабель, подключенный к текущему порту
+        cable = Cable.objects.filter(
             _terminations__object_id=current_port.id,
             _terminations__content_type=ContentType.objects.get_for_model(current_port)
-        ).prefetch_related('_terminations')
+        ).first()
         
-        if not cables:
-            # Конечная точка пути
-            return current_path
+        if not cable:
+            break
             
-        for cable in cables:
-            # Находим противоположный конец кабеля
-            for termination in cable._terminations.all():
-                if termination.object_id != current_port.id:
-                    term_model = termination.content_type.model_class()
-                    other_end = term_model.objects.get(id=termination.object_id)
-                    
-                    new_path = current_path + [{
-                        'cable': cable,
-                        'port_a': current_port,
-                        'port_b': other_end,
-                        'device_a': current_port.device,
-                        'device_b': other_end.device if hasattr(other_end, 'device') else None
-                    }]
-                    
-                    # Если это выходная розетка, ищем подключенный к ней порт
-                    if isinstance(other_end, PowerOutlet):
-                        connected_port = PowerPort.objects.filter(
-                            _connected_ports__object_id=other_end.id,
-                            _connected_ports__content_type=ContentType.objects.get_for_model(other_end)
-                        ).first()
-                        
-                        if connected_port:
-                            queue.append((connected_port, new_path))
-                    
-                    return new_path
-                    
+        # Находим противоположный конец кабеля
+        for termination in cable._terminations.all():
+            if termination.object_id != current_port.id:
+                term_model = termination.content_type.model_class()
+                other_end = term_model.objects.get(id=termination.object_id)
+                
+                path.append({
+                    'cable': cable,
+                    'port_a': current_port,
+                    'port_b': other_end,
+                    'device_a': current_port.device,
+                    'device_b': other_end.device if hasattr(other_end, 'device') else None
+                })
+                
+                # Если это выходная розетка, ищем подключенный к ней порт
+                if isinstance(other_end, PowerOutlet):
+                    connected_port = PowerPort.objects.filter(
+                        _connected_ports__object_id=other_end.id,
+                        _connected_ports__content_type=ContentType.objects.get_for_model(other_end)
+                    ).first()
+                    if connected_port:
+                        current_port = connected_port
+                        break
+                else:
+                    current_port = None
+                break
+        else:
+            break
+            
+        if not current_port:
+            break
+            
     return path
