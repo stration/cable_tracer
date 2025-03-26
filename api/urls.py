16dd944b716cb0159c1ce40933abb20fc6d0da07
stration <34169333+stration@users.noreply.github.com>
2025-03-26 13:19:38 +0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,6 @@
-# urls.py
 from django.urls import path
 from .views import CableTracertViewSet
 
 urlpatterns = [
     path('trace/', CableTracertViewSet.as_view({'get': 'trace'}), name='trace'),
-    path('paths/<int:pk>/', CablePathDetailView.as_view(), name='path-detail'),
 ]
